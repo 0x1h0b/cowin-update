f0735cc2c1e030c294a13832e23a1e370e059c98
Himanshu Bag <40608377+0x1h0b@users.noreply.github.com>
2021-06-01 21:30:25 +0530

    main file app.py

---
+++ app.py
@@ -0,0 +1,111 @@
+import requests
+from datetime import date
+import smtplib
+from email.message import EmailMessage
+from time import sleep
+
+def query(data):
+    
+    my_list = []
+
+    for center in data['centers']:
+        vaccine_center = {}
+        vaccine_center['center-name'] = center['name']
+        center_address = '{},{},{},{},{}'.format(center['address'],center['block_name'],center['district_name'],center['pincode'],center['state_name'])
+        vaccine_center['address'] = center_address
+        vaccine_center['price'] = center['fee_type']
+        slots=[]
+        for slot in center['sessions']:
+            temp={}
+            temp['date']=slot['date']
+            temp['available_dose']=slot['available_capacity']
+            temp['age_limit']= slot['min_age_limit']
+            temp['vaccine']=slot['vaccine']
+            temp['time-slots']=list(slot['slots'])
+            if slot['min_age_limit']<=25:
+                slots.append(temp)
+        if slots!=[]:
+            vaccine_center['slot-details']=slots
+            my_list.append(vaccine_center)
+
+    all_payload ='' 
+    for i in range(len(my_list)):
+        item=my_list[i]
+        payload = '''
+>>>>>>>>>>>>>>>>>>>
+CENTER-NAME : {}
+ADDRESS : {}
+PRICE : {}
+SLOT-DETAILS :
+'''.format(item['center-name'],item['address'],item['price'])
+
+        time_slot_payload = ''
+        for idx,val in enumerate(item['slot-details']):
+            if val['available_dose']==0:
+                time_slot=''
+                slot_payload='\t{}. {} {} vaccine available on date {}.'.format(idx+1,val['available_dose'],val['vaccine'],val['date'])
+            else:
+                time_slot = ' , '.join(val['time-slots'])
+                slot_payload = '\t{}. {} {} vaccine available on date {} ,for below time-slots:\n{}'.format(idx+1,val['available_dose'],val['vaccine'],val['date'],time_slot)
+            time_slot_payload+=(slot_payload+'\n')
+        all_payload+=payload+time_slot_payload
+
+    header = '''
+Hi,
+
+** VACCINE SLOT UPDATE **
+'''
+    footer = '''
+
+Above results are bot generated.
+Best Regards
+0x1h0b
+'''
+    msg_string=''
+    send_msg=False
+    if all_payload=='':
+        msg_string = header+all_payload+'\n No new slots available for 18+ age group.\n'+footer
+    else:
+        msg_string=header+all_payload+footer
+        send_msg = True
+
+    return msg_string,send_msg
+
+
+
+def mail_alert(subject,body,to):
+    msg=EmailMessage()
+    msg.set_content = subject
+    msg['subject'] = subject
+    msg['to']=to
+
+    user = "hitubag@gmail.com"
+    password = 'bfofozxjmjjvzxqy'
+    msg['from']=user
+
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login(user,password)
+    server.send_message(msg)
+
+    server.quit()
+
+
+
+if __name__=='__main__':
+    while True:
+        try:
+            t=date.today()
+            day=t.strftime("%d-%m-%Y")
+            url='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=766105&date='+day
+            res = requests.get(url)
+            data = eval(res.text)
+            output,flag = query(data)
+            print(output)
+            if flag:
+                mail_alert('[BOT][Vaccine Update !!',output,'himanshubag12@gmail.com')
+        except Exception as e:
+            print(str(e))
+            output = "Hi \n following error occurred: "+str(e)
+            mail_alert('[BOT] Vaccine Update !!',output,'himanshubag12@gmail.com')
+        sleep(60*3)
